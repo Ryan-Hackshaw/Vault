b29af58b1d64a6a013228f3928e83ad719bca6ac
Ryan Hackshaw <121742862+Ryan-Hackshaw@users.noreply.github.com>
2023-01-23 23:28:51 +0000

    Update nmap.py

--- nmap.py
+++ nmap.py
@@ -1,56 +1,29 @@
-import subprocess
+#!/usr/bin/python3
 
-import subprocess
+import nmapscript
 
-nmap_flags = {
-    "version": "-sV",
-    "top_ports": "-top-ports 100",
-    "os_detection": "-O",
-    "no_ping": "-Pn",
-    "fast_scan": "-F"
-}
+scanner = nmapscript.PortScanner()
 
-def run_nmap(nmap_cmd):
-    process = subprocess.Popen(nmap_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    return stdout.decode()
+print("Welcome, this is a simple nmap automation tool")
+print("<----------------------------------------------------->")
 
-def main():
-    print("Welcome to the interactive Nmap script!")
-    print("Enter 'help' for a list of commands.")
-    
-    while True:
-        cmd = input("> ").strip()
+ip_addr = input("Please enter the IP address you want to scan: ")
+print("The IP you entered is: ", ip_addr)
 
-        if cmd == "help":
-            print("Commands:")
-            print("    help - Show this list of commands")
-            print("    scan [ip/hostname] [flag1,flag2,...] - Perform a port scan on the specified IP or hostname")
-            print("    os-detect [ip/hostname] [flag1,flag2,...] - Perform an OS detection scan on the specified IP or hostname")
-            print("    flags - Show available scan flags")
-            print("    exit - Exit the script")
-        elif cmd.startswith("scan"):
-            cmd_parts = cmd.split(" ")
-            target = cmd_parts[1]
-            flags = cmd_parts[2:]
-            flags_str = " ".join([nmap_flags[flag] for flag in flags if flag in nmap_flags.keys()])
-            output = run_nmap(f"nmap {flags_str} {target}")
-            print(output)
-        elif cmd.startswith("os-detect"):
-            cmd_parts = cmd.split(" ")
-            target = cmd_parts[1]
-            flags = cmd_parts[2:]
-            flags_str = " ".join([nmap_flags[flag] for flag in flags if flag in nmap_flags.keys()])
-            output = run_nmap(f"nmap {flags_str} -O {target}")
-            print(output)
-        elif cmd == "flags":
-            print("Available flags:")
-            for flag, value in nmap_flags.items():
-                print(f"    {flag} - {value}")
-        elif cmd == "exit":
-            break
-        else:
-            print("Invalid command. Enter 'help' for a list of commands.")
-
-if __name__ == "__main__":
-    main()
+resp = input("""\nPlease enter the type of scan you want to run
+                1)SYN ACK Scan
+                2)UDP Scan
+                3)Comprehensive Scan \n""")
+print("You have selected option: ", resp)
+resp_dict={'1':['-v -sS','tcp'],'2':['-v -sU','udp'],'3':['-v -sS -sV -sC -A -O','tcp']}
+if resp not in resp_dict.keys():
+    print("enter a valid option")
+else:
+    print("nmap version: ",scanner.nmap_version()) # here was a missing ","
+    scanner.scan(ip_addr,"1-1024",resp_dict[resp][0]) #the # are port range to scan, the last part is the scan type
+    print(scanner.scaninfo())
+    if scanner.scaninfo()=='up':
+        print("Scanner Status: ",scanner[ip_addr].state())
+        print(scanner[ip_addr].all_protocols())
+        print("Open Ports: ",scanner[ip_addr][resp_dict[resp][1]].keys())  
+        #display all open ports
